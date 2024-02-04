2407b21fb1ca87e735a5c516a97bdcf1f2cc4353
Sandeep Yadav <86369280+sandeep-2608@users.noreply.github.com>
2024-02-05 00:28:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ import categoryRoutes from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import cors from "cors";
 import path from "path";
-
+import { fileURLToPath } from "path";
+ 
 // configure env
 dotenv.config();
+
+// esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // rest object
 const app = express();
